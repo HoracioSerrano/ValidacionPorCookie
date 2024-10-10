c70e5f963e4261e2d40e318b3ace0d14311d7576
Serrano Horacio <serranohoraciohector@gmail.com>
2024-10-10 18:19:26 -0300

    Agrego Endpoints

---
+++ ValidacionPorCookie/Controllers/Cookie.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ValidacionPorCookie.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class Cookie : Controller
+    {
+        [HttpGet]
+        public IActionResult GET()
+        {
+            return Ok("Cookie seteada");
+        }
+
+
+        [HttpPost]
+        public IActionResult POST()
+        {
+            return Ok("Acceso Concedido");
+        }
+
+    }
+}
